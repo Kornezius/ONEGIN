1c91fa116390264f1399890ea9d1fea4048518bb
Kornezius <atr.pon@yandex.ru>
2026-09-16 22:45:27 +0300

    Add files via upload

--- Onegin_main.cpp
+++ Onegin_main.cpp
@@ -2,16 +2,22 @@
 
 int main()
 {
-    char c;
+    int c;
     FILE * fp = fopen("ONEGIN.txt", "r");
-    while (fp)
+    if (fp)
     {
         int num_symb = 0;
-        while ((c = getc(fp)) != '\n')
+        while ((c = getc(fp)) != EOF)
         {
+            if (c == '\n')
+            {
+                printf("%d\n",num_symb);
+                num_symb = -1;
+
+            }
             num_symb++;
         }
         printf("%d\n",num_symb);
-        num_symb = 0;
+
     }
 }
